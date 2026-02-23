0adb400d268cfb38cb8d4d316d48ddb0a07dab87
vizay2 <vizay.yadav.2@gmail.com>
2026-02-23 15:05:47 +0530

    Update assignment2.sql

--- assignment2.sql
+++ assignment2.sql
@@ -1,11 +1,11 @@
 
 CREATE TABLE Department (
-    dept_id VARCHAR(10) PRIMARY KEY,
+    dept_id VARCHAR(11) PRIMARY KEY,
     dept_name VARCHAR(100) NOT NULL,
     office_location VARCHAR(50)
 );
 CREATE TABLE Faculty (
-    faculty_id VARCHAR(10) PRIMARY KEY,
+    faculty_id VARCHAR(11) PRIMARY KEY,
     faculty_name VARCHAR(100) NOT NULL,
     designation VARCHAR(50),
     email VARCHAR(100),
@@ -13,26 +13,26 @@ CREATE TABLE Faculty (
     FOREIGN KEY (dept_id) REFERENCES Department(dept_id)
 );
 CREATE TABLE Course (
-    course_id VARCHAR(10) PRIMARY KEY,
+    course_id VARCHAR(11) PRIMARY KEY,
     course_name VARCHAR(100) NOT NULL,
     credits INT,
-    dept_id VARCHAR(10),
-    faculty_id VARCHAR(10),
+    dept_id VARCHAR(11),
+    faculty_id VARCHAR(11),
     FOREIGN KEY (dept_id) REFERENCES Department(dept_id),
     FOREIGN KEY (faculty_id) REFERENCES Faculty(faculty_id)
 );
 CREATE TABLE Student (
-    student_id VARCHAR(10) PRIMARY KEY,
+    student_id VARCHAR(11) PRIMARY KEY,
     student_name VARCHAR(100) NOT NULL,
     dob DATE,
-    gender CHAR(1),
+    gender CHAR(10),
     contact_no VARCHAR(10),
     dept_id VARCHAR(10),
     FOREIGN KEY (dept_id) REFERENCES Department(dept_id)
 );
 CREATE TABLE Enrollment (
-    student_id VARCHAR(10),
-    course_id VARCHAR(10),
+    student_id VARCHAR(11),
+    course_id VARCHAR(11),
     semester INT,
     grade VARCHAR(5),
     PRIMARY KEY (student_id, course_id),
